7e0209b99b5fe41ab3af03e0b6d1edd937ccf1f8
markofilipovic2762 <markofilipovicsd@gmail.com>
2020-10-11 01:55:54 +0200

    10.10 login register user

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -36,7 +36,7 @@ const CartScreen = ({ match, location, history }) => {
                 {cartItems.length === 0 ? <Message>Your cart is empty <Link to='/'>Go Back</Link> </Message>
                     : <ListGroup variant='flush'>
                         {cartItems.map(item => (
-                            <ListGroup.Item>
+                            <ListGroup.Item key={item.product}>
                                 <Row>
                                     <Col md={2}>
                                         <Image src={item.image} alt={item.name} fluid rounded />
