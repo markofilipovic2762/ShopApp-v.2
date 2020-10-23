e7ceb20180cd76fcd985a5117bef173f3806bb4a
markofilipovic2762 <markofilipovicsd@gmail.com>
2020-10-23 23:50:42 +0200

    Admin users list and edit

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from '../controllers/userController.js'
+import {
+    authUser, deleteUser, getUserProfile, getUsers,
+    registerUser, updateUserProfile,
+    getUserById, updateUser
+} from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
@@ -9,6 +13,11 @@ router
     .route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
+router
+    .route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser)
 
 
 export default router
