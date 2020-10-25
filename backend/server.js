628836136ed559443e247c061f96da0d02141050
markofilipovic2762 <markofilipovicsd@gmail.com>
2020-10-25 16:41:39 +0100

    24.10 edit product and server upload

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
@@ -6,6 +7,7 @@ import connectDB from './config/db.js'
 import productRoute from './routes/productRoute.js'
 import userRoute from './routes/userRoute.js'
 import orderRoute from './routes/orderRoute.js'
+import uploadRoute from './routes/uploadRoute.js'
 
 dotenv.config()
 
@@ -22,10 +24,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoute)
 app.use('/api/users', userRoute)
 app.use('/api/orders', orderRoute)
+app.use('/api/upload', uploadRoute)
 
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound, errorHandler)
 
